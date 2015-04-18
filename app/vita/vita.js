d54cf5ac0d08f5997425d9ddd69d9be1b6e1c9c1
fonzerelly <ChristianHoerauf@web.de>
2015-04-18 22:43:13 +0200

    standard select implemented

--- app/vita/vita.js
+++ app/vita/vita.js
@@ -8,10 +8,26 @@ angular.module('customizedVita.vita', ['ngRoute'])
       controllerAs: 'vita'
     });
   }])
+  .directive('cvJobSelect', function() {
+    return {
+      restrict: "E",
+      templateUrl: "vita/job-select.html",
+      scope: {
+        jobs: '=',
+        currentJob: '='
+      },
+      link: function(scope, element, attr) {
+        console.log(scope);
+      }
+    };
+  })
   .controller('VitaCtrl', ['$scope', 'VitaLoader', function($scope, VitaLoader) {
     var self = this;
     self.header = 'Lebenslauf';
     $scope.$watch(VitaLoader.getVita, function (vita) {
       angular.extend(self, vita);
+      if (self.jobs!==undefined) {
+        self.currentJob = self.jobs[1];
+      }
     });
   }]);
